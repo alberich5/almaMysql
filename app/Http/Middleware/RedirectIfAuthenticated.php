864a0c975aa32c0f00596d17cde3e05839817733
Omar Zarate Hipolito <alberich_sid@hotmail.com>
2017-07-05 11:35:12 -0500

    Se estan realizando pruebas en los middlewares

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -5,7 +5,6 @@ namespace Omar\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 use Omar\User;
-use Session;
 
 class RedirectIfAuthenticated
 {
@@ -17,33 +16,12 @@ class RedirectIfAuthenticated
      * @param  string|null  $guard
      * @return mixed
      */
-    protected $auth;
-    protected $redirectPath;
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
 
-            switch ($this->auth->user()->idrol) {
-           case '1':
-               # Administrador
-                #return  redirect()->to('admin');
-               break;
-            case '2':
-               # Responsable de agregar productos
-                #return  redirect()->to('responsable');
-                 return redirect()->to('admin')->with('redirectPath', '/');
-               break;
-            default:
-            dd("NO se encontro la pagina solicitada");
-                break;
-           
-       }
-
-
-
-         //return redirect('/almacen-venta');
-
-
+            dd("Se esta entrando el Redirect Auth");
+         return redirect('/almacen-venta');
         }
 
         return $next($request);
