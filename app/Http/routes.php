74be5e287fb547712148109bf03923c311e93a7a
Omar Zarate Hipolito <alberich_sid@hotmail.com>
2017-07-05 13:04:45 -0500

    guadar cmabios inecesarios para que se pueda acceder a otra vesion del sistemas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,7 @@ Route::group(['middleware'=>['auth','admin'],'prefix'=>'admin'],function()
 {
 
 	Route::get('/', function () {    
-		return "Se encuentra de la ruta de Administrador";
+		return "Se dirigio a la ruta de Administrador";
 	});
 
 	 //ruta de categoria
